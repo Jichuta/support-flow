9b3948e275d5c990b842a36de291b9adf87e2842
alejandrosardinasassuresoft <alejandro.sardinas@assuresoft.com>
2026-07-23 11:45:20 -0400

    feat(frontend): implement SupportRequestForm view

--- frontend/src/views/RequestNew.vue
+++ frontend/src/views/RequestNew.vue
@@ -1,6 +1,6 @@
+<script setup>
+import SupportRequestForm from '@/components/SupportRequestForm.vue'
+</script>
 <template>
-  <div>
-    <h1>New Support Request</h1>
-    <p>Request form view coming soon.</p>
-  </div>
+  <SupportRequestForm />
 </template>
